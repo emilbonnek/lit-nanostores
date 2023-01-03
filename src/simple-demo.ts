62b63ff865136bd06814959bed3d40f24b3ad867
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2023-01-03 16:11:34 +0100

    renamed file to StoreController.ts

--- src/simple-demo.ts
+++ src/simple-demo.ts
@@ -1,7 +1,7 @@
 import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators.js";
 
-import { StoreController } from "./nanostores/lit/useStore";
+import { StoreController } from "./nanostores/lit/StoreController";
 
 import { atom } from "nanostores";
 export const isCartOpen = atom(false);
