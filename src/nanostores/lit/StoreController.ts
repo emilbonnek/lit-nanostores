8af2ff5d131f8cab9b9795fb4575e6334d407a34
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2023-01-03 16:14:01 +0100

    using getter instead of duplicating atom value

--- src/nanostores/lit/StoreController.ts
+++ src/nanostores/lit/StoreController.ts
@@ -4,14 +4,15 @@ import { WritableAtom } from "nanostores";
 export class StoreController<AtomType> implements ReactiveController {
   private _atom: WritableAtom<AtomType>;
   private _unsubscribe: () => void;
-  public value: AtomType;
+
+  get value() {
+    return this._atom.get();
+  }
 
   constructor(host: ReactiveControllerHost, atom: WritableAtom<AtomType>) {
     this._atom = atom;
-    this.value = this._atom.get();
     host.addController(this);
-    this._unsubscribe = this._atom.subscribe((value) => {
-      this.value = value;
+    this._unsubscribe = this._atom.subscribe(() => {
       host.requestUpdate();
     });
   }
