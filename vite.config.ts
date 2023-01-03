23000c929b49096cbc3d9c27797461d6cc8451d0
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2023-01-03 05:15:56 +0100

    Fix for build

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: "src/my-app.ts",
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: /^lit/,
-    },
-  },
+  build: {},
 });
