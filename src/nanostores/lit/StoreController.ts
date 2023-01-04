cdd27c9f8686d1f373c2e53ce9ab50a79bf57866
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2023-01-04 20:37:47 +0100

    improvement to StoreController
    Co-Authored-By: Thomas Broyer <173813+tbroyer@users.noreply.github.com>

--- src/nanostores/lit/StoreController.ts
+++ src/nanostores/lit/StoreController.ts
@@ -2,23 +2,27 @@ import { ReactiveController, ReactiveControllerHost } from "lit";
 import { WritableAtom } from "nanostores";
 
 export class StoreController<AtomType> implements ReactiveController {
-  private _atom: WritableAtom<AtomType>;
-  private _unsubscribe: () => void;
+  private _unsubscribe: undefined | (() => void);
 
   get value() {
     return this._atom.get();
   }
 
-  constructor(host: ReactiveControllerHost, atom: WritableAtom<AtomType>) {
-    this._atom = atom;
-    host.addController(this);
+  constructor(
+    private _host: ReactiveControllerHost,
+    private _atom: WritableAtom<AtomType>
+  ) {
+    _host.addController(this);
+  }
+
+  hostConnected() {
     this._unsubscribe = this._atom.subscribe(() => {
-      host.requestUpdate();
+      this._host.requestUpdate();
     });
   }
 
   // Unsubscribe from the store when the host disconnects
   hostDisconnected() {
-    this._unsubscribe!();
+    this._unsubscribe?.();
   }
 }
